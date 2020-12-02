5d81da6b613a253e1caaf797736be9dd7eb3757c
armin <armin536@gmail.com>
2020-12-02 21:52:31 +0330

    Updated libraries

--- buildSrc/src/main/java/Libs.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("unused")
-
-object Libs {
-    // Kotlin
-    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
-
-    // Blueprint
-    const val blueprint = "dev.jahir:Blueprint:${Versions.blueprint}@aar"
-}
--- buildSrc/src/main/java/MyApp.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress("unused")
-
-object MyApp {
-    const val appId = "co.aseman.ubuntu_touch_icons"
-    const val version = 13
-    const val versionName = "0.2.3"
-}
